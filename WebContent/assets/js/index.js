e331ed9c3687ae549e437c49c765c3b854494c08
Ramandeep Singh Randhawa <r.singhrandhawa13@gmail.com>
2024-02-12 15:16:41 +0530

    added menu content for small screen

---
+++ WebContent/assets/js/index.js
@@ -0,0 +1,8 @@
+function appearProfileCard(x){
+    if(x==1){
+        document.getElementById("menu-content").style.display = "block";
+    }
+    else{
+        document.getElementById("menu-content").style.display = "none";
+    }
+}
